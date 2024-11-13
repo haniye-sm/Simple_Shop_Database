4d37c1cb1314b313d4d71cee5fe33487f13700b0
Haniye Safraei Moghadam <hsmoghadamg@gmail.com>
2024-11-13 14:16:55 +0330

    insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,43 @@
+--ورود اطلاعات
+INSERT INTO CUSTOMERS
+VALUES
+('ali rezaei' , 'ali@gmail.com' , '2022-05-10'),
+('sara jafari' , 'sara@gmail.com' , '2021-08-15'),
+('hamed ahmadi' , 'hamed@gmail.com' , '2023-01-20'),
+('haniye karimi' , 'haniye@gmail.com' , '2022-11-01');
+
+
+
+
+INSERT INTO PRODUCTS
+VALUES
+('Organic Honey' , '1 KG' , 7000000),
+('Organic Tea' , '1 KG' , 4500000),
+('Olive Oil' , 'Bottle' , 10000000),
+('Fresh Almonds' , '1 kg' , 6500000);
+
+
+
+
+INSERT INTO ORDERS
+VALUES
+(1 , '2023-02-15' , 7000000 , 1),
+(2 , '2023-03-01' , 6500000 , 4),
+(3 , '2023-01-25' , 4500000 , 2),
+(1 , '2023-01-27' , 7000000 , 1),
+(4 , '2023-06-05' , 10000000 , 3),
+(2 , '2023-07-21' , 4500000 , 2);
+
+
+
+
+
+
+INSERT INTO RATINGS
+VALUES
+(1 ,1 ,5, '2023-02-16'),
+(4 ,2 ,4, '2023-03-02'),
+(2 ,3 ,5, '2023-01-26'),
+(1 ,1 ,3, '2023-05-11'),
+(3 ,4 ,4, '2023-06-06'),
+(2 ,2 ,5, '2023-03-02');
